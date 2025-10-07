3fccbf7e67e3c1f4ccda19e5fcd171acffe6564b
ChrisPoul <chrispoulsenfernandez@gmail.com>
2025-10-07 10:14:34 -0600

    Removed cotizaciones page since it is not needed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,6 @@
 
 	const navLinks = [
 		{ href: '/', label: 'Dashboard', icon: 'dashboard.svg' },
-		{ href: '/cotizaciones', label: 'Cotizaciones', icon: 'cotizaciones.svg' },
 		{ href: '/servicios', label: 'Servicios', icon: 'servicios.svg' },
 		{
 			label: 'Usuarios',
